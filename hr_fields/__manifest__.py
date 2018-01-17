d11cd2a5bfa1688770ea432500e357134839a02a
Raul Romero <rromero3@gmail.com>
2018-01-17 00:37:30 -0400

    udated version

--- hr_fields/__manifest__.py
+++ hr_fields/__manifest__.py
@@ -16,7 +16,7 @@
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
     # for the full list
     'category': 'Tools',
-    'version': '0.105',
+    'version': '0.180117',
 
     # any module necessary for this one to work correctly
     'depends': ['base','hr'],
