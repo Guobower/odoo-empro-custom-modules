065e4e513668246ed7a4d8e68b1a80e9631bfd9f
Raul Romero <rromero3@gmail.com>
2018-01-17 00:40:20 -0400

    gdrive_url field added

--- hr_fields/models/models.py
+++ hr_fields/models/models.py
@@ -12,10 +12,9 @@ class HrEmployee(models.Model):
 	personal_phone = fields.Char('Telefono Personal')
 	personal_mobile = fields.Char('Celular Personal')
 	CNS_afiliado = fields.Boolean('Afiliado en la CNS')
-
 	gdrive_url = fields.Char('Url File en GDrive')
 
-	
+
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
 
