5e6f842c0e07ff902ee3b92a887706519e6a3bc6
Raul Romero <rromero3@gmail.com>
2018-01-17 20:41:29 -0400

    create empro hr fieldss

---
+++ empro_hr_fields/__manifest__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "HR Fields Empro Ltda - V2",
+
+    'summary': """
+        Campos agregados para poder ser utilizados en la parte de RRHH de EMPRO LTDA kanban""",
+
+    'description': """
+        Campos agregados para poder ser utilizados en la parte de RRHH de EMPRO LTDA. Odoo V11 kanban $ views
+    """,
+
+    'author': "Empro Ltda",
+    'website': "http://www.emproltda.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Tools',
+    'version': '0.18011708',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base','hr'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+    'installable': True,
+}
---
+++ empro_hr_fields/models/models.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class HrEmployee(models.Model):
+	_inherit = 'hr.employee'
+	#_name = 'hr_fields.hr_fields'
+
+	x_employee_code = fields.Char('Codigo Empro', default="EMP_")
+	x_legal_name =  fields.Char('Nombre Legal')
+	x_personal_email = fields.Char('Email Personal')
+	x_personal_phone = fields.Char('Telefono Personal')
+	x_personal_mobile = fields.Char('Celular Personal')
+	x_cns_afiliado = fields.Boolean('Afiliado en la CNS')
+	x_google_drive_url = fields.Char('Url File en GDrive')
+
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+
+# class test_module(models.Model):
+#     _name = 'test_module.test_module'
+
+#     name = fields.Char()
+#     value = fields.Integer()
+#     value2 = fields.Float(compute="_value_pc", store=True)
+#     description = fields.Text()
+#
+#     @api.depends('value')
+#     def _value_pc(self):
+#         self.value2 = float(self.value) / 100
