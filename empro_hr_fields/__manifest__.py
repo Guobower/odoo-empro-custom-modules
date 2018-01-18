830dcfb48f8b3df4de50f48413435644dde48119
Raul Romero <rromero3@gmail.com>
2018-01-17 22:54:57 -0400

    updated view releasse13

--- empro_hr_fields/__manifest__.py
+++ empro_hr_fields/__manifest__.py
@@ -7,6 +7,8 @@
 
     'description': """
         Campos agregados para poder ser utilizados en la parte de RRHH de EMPRO LTDA. Odoo V11 kanban $ updated fields new update
+
+        view updated
     """,
 
     'author': "Empro Ltda",
@@ -16,7 +18,7 @@
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
     # for the full list
     'category': 'Tools',
-    'version': '0.180117012',
+    'version': '0.180117013',
 
     # any module necessary for this one to work correctly
     'depends': ['base','hr'],
